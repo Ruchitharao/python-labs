b4731ba2e44b3576a7a74f279a70156de68c32da
Ruchitharao <raoruchi.preetham@gmail.com>
2022-06-17 17:00:22 -0700

    dumped dataframe data into postgres by functional calls

--- data_transpose.py
+++ data_transpose.py
@@ -1,16 +1,21 @@
 import pandas as pd
+from db import execute_values
 
-df= pd.read_csv('managers_data.csv')
+
+def main():
+    df = pd.read_csv('managers_data.csv')
+    manager = []
+    empid = []
+    for inx, row in df.iterrows():
+        manager_name = row[0]
+        empids = list(row[1:].values)
+        manager += [manager_name] * len(empids)
+        empid += empids
+
+    df_emp_transpose = pd.DataFrame({'Manager': manager, 'EmpId': empid})
+    print(df_emp_transpose)
+    execute_values(df_emp_transpose, "managers")
 
 
-manager = []
-empid = []
-
-for inx, row in df.iterrows():
-    manager_name = row[0]
-    empids = list(row[1:].values)
-    manager += [manager_name]*len(empids)
-    empid += empids
-
-df_emp_transpose = pd.DataFrame({'Manager':manager, 'Emp Ids': empid})
-print(df_emp_transpose)
+if __name__ == "__main__":
+    main()
--- db.py
+++ db.py
@@ -1,21 +1,34 @@
 import psycopg2
-
-#connect to the database
-
-db = psycopg2.connect(
-    host = "localhost",
-    user= "postgres",
-    password="password",
-    port=5432
-)
+import psycopg2.extras as extras
 
 
-#cursor and table creation
-mycursor = db.cursor()
 
-#mycursor.execute("CREATE TABLE managers(Manager varchar(30), EmpId integer)")
+def execute_values(df, table):
+    conn = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="password",
+        port=5432
+    )
 
-#mycursor.execute("SHOW")
-mycursor.execute("SELECT * FROM managers")
-#connection close
-db.close()
+    # mycursor.execute("CREATE TABLE managers(Manager varchar(30), EmpId integer)")
+
+    tuples = [tuple(x) for x in df.to_numpy()]
+
+    cols = ','.join(list(df.columns))
+    # SQL query to execute
+    query = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("the dataframe is inserted")
+    cursor.close()
+
+
+
