23493da58b201bbc48bc7f4ce8f1f719882117c7
Ruchitharao <raoruchi.preetham@gmail.com>
2022-06-18 12:04:12 -0700

    Converted managers column into camel case

--- data_transpose.py
+++ data_transpose.py
@@ -12,7 +12,12 @@ def main():
         manager += [manager_name] * len(empids)
         empid += empids
 
+    # data = pd.DataFrame(manager, columns=['Manager'])
+    # data["Manager"] = data["Manager"].str.upper().str.title()
+    # manager = data["manager"].values.tolist()
+
     df_emp_transpose = pd.DataFrame({'Manager': manager, 'EmpId': empid})
+    df_emp_transpose["Manager"]= df_emp_transpose["Manager"].str.upper().str.title()
     print(df_emp_transpose)
     execute_values(df_emp_transpose, "managers")
 
